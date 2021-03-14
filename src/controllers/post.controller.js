2efb1d563c3edf958cd083a426810bd269bcf0d9
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-14 08:10:38 +0530

    added /GET posts & postById

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -16,3 +16,24 @@ export const newPost = async (req, res) => {
     return res.status(500).json({ message: 'Could not create post', error: err });
   }
 };
+
+export const getPosts = async (_req, res) => {
+  try {
+    const posts = await Post.find({}, { __v: 0 });
+    return res.status(200).json({ message: 'All posts', data: posts });
+  } catch (error) {
+    Logger.error('Error retrieving posts', error);
+    return res.status(500).json({ message: 'Error retrieving posts', error });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    return res.status(200).json({ message: 'Post retrieved', data: post });
+  } catch (error) {
+    Logger.error('Error retrieving post', error);
+    return res.status(500).json({ message: 'Error retrieving post', error });
+  }
+};
--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { newPost } from '../controllers/post.controller';
+import { getPostById, getPosts, newPost } from '../controllers/post.controller';
 
 const router = Router();
 
 router.post('/new', newPost);
+router.get('/all', getPosts);
+router.get('/:id', getPostById);
 
 export default router;
