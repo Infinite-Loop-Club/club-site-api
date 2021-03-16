0b0417809daed0034c97df28804c8be8c325cf0f
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-16 21:08:37 +0530

    added error handling for users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,14 +13,37 @@ import { User } from '../models';
 export const newUser = async (req, res) => {
   const { body } = req;
   Logger.debug('Acknowledged: ', body);
-  const theNewUser = new User(body);
+  const mandatoryFields = ['name', 'registerNumber', 'email', 'phoneNumber', 'year'];
 
   try {
+    for (const mandatoryField of mandatoryFields) {
+      if (!body[mandatoryField]) {
+        return res.status(400).json({
+          message: `Validation Error: '${mandatoryField}' is required`,
+          error: {
+            name: 'ValidationError',
+            keyPattern: {
+              [mandatoryField]: 1
+            },
+            keyValue: {
+              [mandatoryField]: String(body[mandatoryField])
+            }
+          }
+        });
+      }
+    }
+    const theNewUser = new User(body);
     const userDocument = await theNewUser.save();
-    Logger.debug('Registration successfully.');
+    Logger.debug('Registration successful.');
     return res.status(200).json({ message: 'Successfully registered', data: userDocument });
   } catch (err) {
     Logger.debug(err);
-    return res.status(400).json({ message: 'Could not complete registration', error: err });
+    if (err.code === 11000) {
+      const errorKeys = Object.keys(err.keyPattern);
+      return res
+        .status(400)
+        .json({ message: `${body[errorKeys[0]]} is already registered`, error: err });
+    }
+    return res.status(500).json({ message: 'Could not complete registration', error: err });
   }
 };
