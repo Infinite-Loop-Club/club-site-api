766e8d888574522f167765a588a365b76eaafdd2
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-17 08:52:29 +0530

    updated user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,14 +3,16 @@ import { Schema, model } from 'mongoose';
 const UserSchema = new Schema(
   {
     registerNumber: {
-      type: String,
-      minlength: 12,
-      maxlength: 12,
+      type: Number,
+      min: 810000000000,
+      max: 810025999999,
       required: true,
       unique: true
     },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    gender: { type: String, required: true },
+    department: { type: String, required: true, default: 'CSE' },
     phoneNumber: { type: Number, min: 4444444444, max: 9999999999, required: true },
     year: { type: Number, required: true }
   },
