2c9878e9b7f1c8259ef51d726ae7a8acf5fe2d8b
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-26 07:54:24 +0530

    updates from KrishnaMoorty12/babel-express-template

--- src/server.js
+++ src/server.js
@@ -8,12 +8,13 @@ import 'dotenv/config';
 import { requestLogger } from './middlewares';
 import { PostRouter, UserRouter } from './routers';
 
-const { PORT, DEV_DB_SERVER, PROD_DB_SERVER } = process.env;
+const { PORT, HOST, DEV_DB_SERVER, PROD_DB_SERVER, NODE_ENV } = process.env;
 
-const DB_SERVER = process.env.NODE_ENV === 'production' ? PROD_DB_SERVER : DEV_DB_SERVER;
+const DB_SERVER = NODE_ENV === 'production' ? PROD_DB_SERVER : DEV_DB_SERVER;
+const LOGGER_LEVEL = NODE_ENV === 'production' ? Logger.WARN : Logger.DEBUG;
 
 Logger.useDefaults({
-  defaultLevel: process.env.NODE_ENV === 'production' ? Logger.ERROR : Logger.DEBUG,
+  defaultLevel: LOGGER_LEVEL,
   formatter: messages => {
     messages.unshift(
       `[${new Date().toLocaleDateString('en-GB')} ${new Date().toLocaleTimeString()}]`
@@ -21,30 +22,39 @@ Logger.useDefaults({
   }
 });
 
-const server = express();
+const app = express();
 
-server.use(requestLogger);
-server.use(cors());
-server.use(express.json());
+app.use(requestLogger);
+app.use(cors());
+app.use(express.json());
 
-server.use('/user', UserRouter);
-server.use('/post', PostRouter);
+app.use('/user', UserRouter);
+app.use('/post', PostRouter);
 
-server.get('*', (_req, res) => {
+app.get('*', (_req, res) => {
   res.sendStatus(200);
 });
 
-server.listen(PORT, async () => {
-  Logger.info(`Server started at port ${chalk.magenta(PORT)}`);
-  Logger.info(`Listening for requests at: ${chalk.cyan(`http://127.0.0.1:${PORT}`)}`);
+const server = app.listen(PORT, HOST);
+
+server.once('listening', async () => {
+  const { address, port } = server.address();
+  Logger.info(`Server started at port ${chalk.magenta(port)}`);
+  Logger.info(`Listening for requests at: ${chalk.cyan(address + ':' + port)}`);
 
   try {
-    Logger.debug(`Database server is at ${chalk.cyan(DB_SERVER)}.`);
+    Logger.debug(`Database server is at ${chalk.cyan(DB_SERVER)}`);
     const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
     await mongoose.connect(DB_SERVER, options);
-    Logger.info(`Database connection ${chalk.greenBright('successful.')}`);
+    Logger.info(`Database connection ${chalk.greenBright('successful')}`);
   } catch (err) {
     Logger.error(`Database connection ${chalk.redBright('failed.')}`);
     Logger.error(`Error: could not connect to the database at ${DB_SERVER}\n`, err);
   }
+});
+
+server.on('error', err => {
+  Logger.error(chalk.red(err.name));
+  Logger.warn(chalk.yellow(err.message));
+  Logger.info(err.stack);
 });
