e859d36f51c8823b350dd7520e9b91f3f28ae65a
Santhosh <santhoshvelr@yahoo.com>
2021-04-07 19:17:56 +0530

    closed voting and opened registration (#17)
    * closed voting and opened registration
    
    * removed voting related files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,2 @@
 export * as PostController from './post.controller';
 export * as UserController from './user.controller';
-export * as VoteController from './vote.controller';
--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,2 @@
 export { default as Post } from './post.model';
 export { default as User } from './user.model';
-export { default as Vote } from './vote.model';
